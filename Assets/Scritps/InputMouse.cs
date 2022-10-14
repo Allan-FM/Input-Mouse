8c83a126136173298e692f51ffd8e1003d7ad89f
Allan <allanfelipedk@gmail.com>
2022-10-14 20:32:03 -0300

    Input Mouse

---
+++ Assets/Scritps/InputMouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMouse : MonoBehaviour
+{
+    private float speed = 2.5f;
+    private string mouseY = "Mouse Y";
+    private string mouseX = "Mouse X";
+    private void Update()
+    {
+        float inputMouseX = Input.GetAxis(mouseX);
+        float inputMouseY = Input.GetAxis(mouseY);
+
+        transform.Translate(new Vector3(inputMouseX * speed * Time.deltaTime,
+                                        inputMouseY * speed * Time.deltaTime, 0));
+    }
+}
